eae4b2734ab812da57d52922cc6b86cef28f459b
Caner Dabakoğlu <cdabakoglu@gmail.com>
2019-03-06 11:50:07 +0300

    Create Welcome_to_Java.java

---
+++ Welcome_to_Java.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+
+// Caner Dabakoglu
+// GitHub: https://github.com/cdabakoglu
