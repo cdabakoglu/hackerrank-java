9ac2eac07ce335b5ba3ecaff6822c4d7114be266
Caner Dabakoğlu <cdabakoglu@gmail.com>
2019-03-27 13:29:44 +0300

    Create Java_Exception_Handling.java

---
+++ Java_Exception_Handling.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            System.out.println(x/y);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        }
+    }
+}
+
+
+// Caner Dabakoglu
+// GitHub: https://github.com/cdabakoglu
